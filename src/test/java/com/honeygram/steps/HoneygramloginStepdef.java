ac668ca51930d1a3bbc25e3fcfde79d67f2efbc5
tahashinegp <tahashin.egp@outlook.com>
2018-09-05 18:41:38 +0600

    added stepdefinition for login feature

---
+++ src/test/java/com/honeygram/steps/HoneygramloginStepdef.java
@@ -0,0 +1,12 @@
+package com.honeygram.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+
+public class HoneygramloginStepdef {
+    @Then("^User will see dashboard$")
+    public void userWillSeeDashboard() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
